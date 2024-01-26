5202d0a8f5e59eda64219a54d198ab98411708d6
Abhay Deomane <88963060+Abhaydeomane@users.noreply.github.com>
2024-01-26 18:09:21 +0530

    Add files via upload

---
+++ client/src/Homepage.js
@@ -0,0 +1,49 @@
+// BlogList.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom'; // Import useNavigate
+
+const BlogList = () => {
+  const [blogs, setBlogs] = useState([]);
+  const navigate = useNavigate(); // Initialize the useNavigate hook
+
+  useEffect(() => {
+    // Fetch all blogs when the component mounts
+    fetchBlogs();
+  }, []);
+
+  const fetchBlogs = async () => {
+    try {
+      const response = await axios.get('https://blog-application-server-eight.vercel.app/blog/all');
+      setBlogs(response.data);
+    } catch (error) {
+      console.error('Error fetching blogs:', error);
+    }
+  };
+
+  // Function to navigate to the CreateBlogForm page
+  const goToCreateBlog = () => {
+    navigate('/create');
+  };
+
+  return (
+    <div>
+      <h1>Blog Post Application</h1>
+      {/* Button to navigate to the CreateBlogForm page */}
+      <button onClick={goToCreateBlog}>Create Blog</button>
+      <ul>
+        {blogs.map((blog) => (
+          <li key={blog._id}>
+            <Link to={`/blogs/${blog._id}`}>
+              <h5>{blog.title}</h5>
+            </Link>
+            {/* <p>{blog.description}</p>
+            <img src={blog.image} alt={blog.title} /> */}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default BlogList;
---
+++ client/src/UpdateBlogForm.js
@@ -0,0 +1,77 @@
+// UpdateBlogForm.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams, useNavigate } from 'react-router-dom';
+
+const UpdateBlogForm = () => {
+  const { id } = useParams();
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [image, setImage] = useState('');
+  const navigate = useNavigate(); // useNavigate hook for navigation
+  const [isLoading, setIsLoading] = useState(false); // State to track loading status
+
+  useEffect(() => {
+    // Fetch the specific blog when the component mounts
+    fetchBlog();
+  }, []);
+
+  const fetchBlog = async () => {
+    try {
+      const response = await axios.get(`https://blog-application-server-eight.vercel.app/blog/${id}`);
+      const blogData = response.data;
+      setTitle(blogData.title);
+      setDescription(blogData.description);
+      setImage(blogData.image);
+    } catch (error) {
+      console.error('Error fetching blog details for update:', error);
+      // Navigate to home page if the blog is not found
+      navigate('/');
+    }
+  };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      setImage(reader.result);
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleUpdateBlog = async () => {
+    try {
+      setIsLoading(true); // Set loading state to true
+      await axios.put(`https://blog-application-server-eight.vercel.app/update_blog/${id}`,{title,description,image});
+      navigate('/');
+    } catch (error) {
+      console.error('Error updating blog:', error);
+    }finally {
+      setIsLoading(false); // Set loading state to false regardless of success or failure
+    }
+  };
+
+  return (
+    <div>
+      <h2>Update Blog Form</h2>
+      <label>Title:</label>
+      <textarea value={title} onChange={(e) => setTitle(e.target.value)} />
+      <br />
+      <label>Description:</label>
+      <textarea value={description} onChange={(e) => setDescription(e.target.value)} />
+      <br />
+      {/* Input for image upload */}
+      <label>Image:</label>
+      <input type="file" onChange={handleImageChange} />
+      <br />
+      <button onClick={handleUpdateBlog}>Update Blog</button>
+      {isLoading && <div>Loading...</div>}
+    </div>
+  );
+};
+
+export default UpdateBlogForm;
